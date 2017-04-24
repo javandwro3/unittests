2e7213b7998e3a722d86347e4a8b35a2befc019e
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-24 20:59:26 +0200

    added gitignore

---
+++ src/main/java/pl/jwrabel/Point.java
@@ -0,0 +1,57 @@
+package pl.jwrabel;
+
+/**
+ * Created by jakubwrabel on 24.04.2017.
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(String text){ // "100,2"
+        String[] splitted = text.split(",");
+    }
+
+    public void add(Point point){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (x != point.x) return false;
+        return y == point.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
