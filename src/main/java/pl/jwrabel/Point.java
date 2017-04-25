cf5cc93db15dc3ae1ec455f93018a845bce01194
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-25 19:33:23 +0200

    added gitignore

--- src/main/java/pl/jwrabel/Point.java
+++ src/main/java/pl/jwrabel/Point.java
@@ -12,19 +12,47 @@ public class Point {
         this.y = y;
     }
 
-
-    public Point(String text){ // "100,2"
+    public Point(String text) { // "100,2"
         String[] splitted = text.split(",");
         // splitted -> { "100", "2" }
 
 
     }
 
-    public void add(int x, int y){
+    public static void main(String[] args) {
+        Point point1 = new Point(100, 200);
+        System.out.println(point1);
 
+        System.out.println("-- PO DODANIU metodą add(int x, int y) ---");
+        point1.add(50, 30);
+        System.out.println(point1);
+
+        // reset
+        point1 = new Point(100, 200);
+        System.out.println("-- po resecie --");
+        System.out.println(point1);
+
+
+        Point point2 = new Point(100, 300);
+        System.out.println("-- punkt2 -- ");
+        System.out.println(point2);
+
+        point1.add(point2);
+        System.out.println("-- po dodaniu metodą add(Point point)");
+        System.out.println(point1);
     }
 
-    public void add(Point point){
+    public void add(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public void add(Point point) {
+        int x2 = point.getX();
+        int y2 = point.getY();
+
+        this.x += x2;
+        this.y += y2;
 
     }
 
